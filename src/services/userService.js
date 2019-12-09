9d3468cae023e155372b9d02e3a82968472705d4
Mateus Arbex <mateusarbex@hotmail.com>
2019-12-09 08:50:38 -0400

    fix userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,4 @@ const userModel = require('../models/user')
 userModel.methods(['get', 'post', 'put', 'delete'])
 userModel.updateOptions({ new: true, runValidators: true })
 
-module.exports = userModel
+module.exports = userService
