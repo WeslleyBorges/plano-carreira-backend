9da3bdbba54fba06b3e222bd4dc81a6d27d058aa
Mateus Arbex <mateusarbex@hotmail.com>
2019-12-13 11:11:08 -0400

    add user auth

---
+++ src/controllers/authController.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken')
+const userSchema = require('../models/user')
+const secret = require('../config/config').secret
+
+module.exports = {
+   login: function(res,req){
+            const user = req.req.body.user
+            const password = req.req.body.senha
+            userSchema.findOne({email:user}).exec((err,login)=>{
+            if(login){
+                if(login.senha === password){
+                    let token = jwt.sign({ user:login.email },secret,{ expiresIn: '24h' })
+                    res.res.json({
+                        success:true,
+                        message:"Autenticação sucedida",
+                        token: token
+                    })
+                }
+                else {
+                    res.res.send({
+                        status:405,
+                        message:"Usuário ou senha incorreta"
+                    })
+                }
+            }
+            else {
+                res.res.send({
+                status:405,
+                message:"Usuário ou senha incorreta"
+                })
+            }
+        })
+      }
+}
---
+++ src/middleware/auth.js
@@ -0,0 +1,35 @@
+let JWT = require('jsonwebtoken')
+const config = require('../config/config')
+
+let checkToken = (req, res, next)=>{
+    let token = req.headers['x-access-token'] || req.headers['authorization']
+    if(token){
+        if (token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
+          }
+        JWT.verify(token,config.secret, (err,decoded)=>{
+            if(err){
+                return res.json({
+                    success: false,
+                    message: "Token inválido"
+                })
+            }
+            else{
+                req.decoded = decoded
+                next()
+            }
+            
+        })
+
+    }
+    else{
+        return res.json({
+            success: false,
+            message: "Token não enviado"
+        })
+    }
+}
+module.exports = {
+    checkToken
+}
