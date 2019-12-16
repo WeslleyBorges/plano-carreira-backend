47a82a817a3e554cc642433fe19a3c03a9509618
Mateus Arbex <mateusarbex@hotmail.com>
2019-12-15 22:15:21 -0400

    cors config update

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,7 +2,7 @@ let JWT = require('jsonwebtoken')
 const config = require('../config/config')
 
 let checkToken = (req, res, next)=>{
-    
+    console.log(req.headers)
     let token = req.headers['x-access-token'] || req.headers['authorization']
     if(token){
         if (token.startsWith('Bearer ')) {
