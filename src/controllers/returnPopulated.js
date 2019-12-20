2123c437269f35b470a8ef3d9faa8965c9f39aaa
Mateus Arbex <mateusarbex@hotmail.com>
2019-12-20 14:13:22 -0400

    func get

--- src/controllers/returnPopulated.js
+++ src/controllers/returnPopulated.js
@@ -3,7 +3,8 @@ const func = require('../models/funcionario')
 module.exports = {
     populateFuncionario: function(req,res,next){
         const pathName = {path:'cargoNivel',populate:[{path:'nivel'},{path:'cargo'}]}
-        func.find({}).exec(function(err,all){
+        const query = req.params.id? func.findById(req.params.id): func.find({})
+        query.exec(function(err,all){
             if(err){
                 res.send(404)
             }
