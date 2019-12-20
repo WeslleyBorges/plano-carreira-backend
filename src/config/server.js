24efbdb284c518f8d4eea39206469f295c1066a1
Mateus Arbex <mateusarbex@hotmail.com>
2019-12-20 17:06:19 -0400

    cors

--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const server = express()
 const bodyParser = require('body-parser')
-const cors = require('./cors')
+const cors = require('cors')
 
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
-server.use(cors)
+server.use(cors())
 
 const port = process.env.PORT || 8080
 server.listen(port, () => console.log(`Server is running on port ${port}`))
--- src/controllers/returnPopulated.js
+++ src/controllers/returnPopulated.js
@@ -1,23 +1,13 @@
-
 const func = require('../models/funcionario')
 module.exports = {
     populateFuncionario: function(req,res,next){
         const pathName = {path:'cargoNivel',populate:[{path:'nivel'},{path:'cargo'}]}
-        const query = req.params.id? func.findById(req.params.id): func.find({})
-        query.exec(function(err,all){
-            if(err){
-                res.send(404)
+        req.quer = req.quer.populate(pathName);
+        req.quer.exec(function(err, one){
+            if(one){
+                res.json(one)
             }
-            func.populate(all,pathName,function(err,result){
-                res.json(
-                    result
-                )
-                if(err){
-                    res.send(500)
-                }
-            })
         })
-        
             
        
     }
