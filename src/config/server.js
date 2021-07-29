3fe78acb09538d04f9521a27ec35cb520682e723
Robervald da Silva Sidrome <rsidrome@gmail.com>
2021-07-29 19:45:32 -0400

    adicionado o CORS no Server

--- src/config/server.js
+++ src/config/server.js
@@ -5,7 +5,13 @@ const cors = require('cors')
 
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
-server.use(cors())
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");  	
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  server.use(cors());
+  next();
+});
+
 
 const port = process.env.PORT || 8080
 server.listen(port, () => console.log(`Server is running on port ${port}`))
